2f60bc2564ce30b1851837368c9f0307b700d13f
pratheeshraj <rajeshnsk5@gmail.com>
2024-03-26 18:13:44 +0530

    add

---
+++ scroll/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Hero from "./components/Hero";
+import ScrollSection from "./components/ScrollSection";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <ScrollSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
